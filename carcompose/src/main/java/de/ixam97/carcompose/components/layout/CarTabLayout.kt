399a0ec32553c960d87e288a2207e96bede93c73
Maximilian Goldschmidt <ixam97@ixam97.de>
2026-05-07 21:25:53 +0200

    Made vertical tab layouts scrollable

--- carcompose/src/main/java/de/ixam97/carcompose/components/layout/CarTabLayout.kt
+++ carcompose/src/main/java/de/ixam97/carcompose/components/layout/CarTabLayout.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -132,6 +134,7 @@ private fun <T> CarTabLayout(
                             top = CarTheme.carDimensions.defaultVerticalPadding
                         )
                         .width(189.dp + CarTheme.carDimensions.defaultHorizontalPadding)
+                        .verticalScroll(rememberScrollState())
                 ) {
                     tabs.subList(0, tabs.size.coerceAtMost(maxTabs)).forEach { tab ->
                         CarTabLayoutVerticalTab(
@@ -168,6 +171,7 @@ private fun <T> CarTabLayout(
                             top = CarTheme.carDimensions.defaultVerticalPadding
                         )
                         .width(IntrinsicSize.Max)
+                        .verticalScroll(rememberScrollState())
                 ) {
                     tabs.subList(0, tabs.size.coerceAtMost(maxTabs)).forEach { tab ->
                         CarTabLayoutVerticalTabCompact(
@@ -189,7 +193,7 @@ private fun <T> CarTabLayout(
                     }
                 }
                 Box(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.weight(1f)//fillMaxSize()
                 ) {
                     content(selectedKey)
                 }
